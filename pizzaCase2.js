66e74d4a28a2f555420e319773d3c363ac58b60a
DESKTOP-4NMMJTU\ADMIN <fern_2546@hotmail.com>
2022-09-14 16:00:48 +0700

    14.9.65 DSI213 Week6 Lab 2 (ต่อ)

---
+++ pizzaCase2.js
@@ -0,0 +1,21 @@
+pizza = ["Hawaiian", "Ham", "Seafood", "Pepperoni", "DoubleCheese"];
+size =  ["small", "medium", "large", "New York" ];
+price = [ [199,239,299,359],
+          [219,259,339,399],
+          [399,439,499,599],
+          [  0,259,299,339],
+          [199,239,259,299]];
+picture = ["image/hawaiian.png","image/ham.png","image/seafood.png",
+           "image/pepperoni.png","image/doublecheese.png"]
+function showImage() {
+   
+    userChoice = pizzaSelected();
+    document.getElementById("pizzaImage").src = picture[userChoice];
+}
+
+function pizzaSelected() {
+    pizzaChosen = document.getElementById("pizzaList");
+    var chosenPizzaIndex = pizzaChosen.selectedIndex - 1; //ลบ1 เพื่อให้ใน cosole ตรงกับ array ซึ่งเริ่มจาก 0
+    //console.log(chosenPizzaIndex);
+    return chosenPizzaIndex;
+}
